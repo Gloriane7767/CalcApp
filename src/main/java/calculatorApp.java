33a68895edffc32cb91855c97691cc10efcfc80f
Glory Foncha <gloriane7767@gmail.com>
2025-03-14 23:30:28 +0100

    Completed the Perform Calculations with switch operator.

--- src/main/java/calculatorApp.java
+++ src/main/java/calculatorApp.java
@@ -18,4 +18,30 @@ public class Calculator { //This defines the calculator class, which contains ou
             System.out.println(" Enter second number: ");  // Ask for the second number.
             double num2 = scanner.nextDouble();  // Read the second number and store it in `num2`.
 
+            // Perform calculation
+            double result;  // A variable to store the result.
+
+            switch (operator) { // This checks which operator the user entered.
+                case '+':
+                    result =num1 + num2; // If "+", add the numbers.
+                    break;
+                case '-':
+                    result = num1-num2; //If "-", subtract the numbers.
+                    break;
+                case '*':
+                    result = num1 * num2; // If "*", multiply the numbers.
+                    break;
+                case '/':
+                    if (num2 != 0) { // if num2 is not equal to zero, perform the division
+                        result = num1/num2;
+                    } else {// else print out the message and continue the loop
+                        System.out.println("Error: Division by zero is not allowed.");
+                        continue; //Restart the loop without showing a result
+                    }
+                    break;
+                default:
+                    System.out.println("Invalid operator. Please try again");// If the operator is wrong.
+                    continue; // Restart the loop without showing a result.
+            }
+
 
